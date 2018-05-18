844ef73be5e6df04f833ccec7ad9f33859c62d0a
DESKTOP-VLKFD84\guyto <turgibot@gmail.com>
2018-05-18 17:39:22 +0300

    start working on registration customization

--- CSA_Project/CSA_Project/Startup.cs
+++ CSA_Project/CSA_Project/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CSA_Project.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Owin;
 
@@ -13,6 +16,53 @@ namespace CSA_Project
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+        // In this method we will create default User roles and Admin user for login
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "guy";
+                user.Email = "turgibot@gmail.com";
+
+                string userPWD = "123456";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("Viewer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Viewer";
+                roleManager.Create(role);
+
+            }
+
         }
     }
 }
