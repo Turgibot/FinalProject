c659597e76a28ace33e54a63d7d25ccd07ad948b
DESKTOP-VLKFD84\guyto <turgibot@gmail.com>
2018-05-20 16:57:24 +0300

    Add setting page model, view and controller add setting model migration

---
+++ CSA_Project/CSA_Project/Controllers/SettingsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CSA_Project.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CSA_Project.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Settings
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
+            return View(db.Settings.ToList());
+        }
+
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        // GET: Settings/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SettingsViewModels settingsViewModels = db.Settings.Find(id);
+            if (settingsViewModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(settingsViewModels);
+        }
+
+        // GET: Settings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Settings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,MaxPeopleAllowed")] SettingsViewModels settingsViewModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Settings.Add(settingsViewModels);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(settingsViewModels);
+        }
+
+        // GET: Settings/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SettingsViewModels settingsViewModels = db.Settings.Find(id);
+            if (settingsViewModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(settingsViewModels);
+        }
+
+        // POST: Settings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,MaxPeopleAllowed")] SettingsViewModels settingsViewModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(settingsViewModels).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(settingsViewModels);
+        }
+
+        // GET: Settings/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SettingsViewModels settingsViewModels = db.Settings.Find(id);
+            if (settingsViewModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(settingsViewModels);
+        }
+
+        // POST: Settings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            SettingsViewModels settingsViewModels = db.Settings.Find(id);
+            db.Settings.Remove(settingsViewModels);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CSA_Project/CSA_Project/Models/SettingsVIewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CSA_Project.Models
+{
+    public class SettingsViewModels
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public long ID { get; set; }
+        [DisplayName("Number of people allowed")]
+        public int MaxPeopleAllowed { get; set; }
+
+    }
+    
+
+}
