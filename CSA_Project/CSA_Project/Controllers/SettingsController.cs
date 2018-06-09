8751e8a9f8bceeb41ded8c20980a206f98baddc3
Guy Tordjman <turgibot@gmail.com>
2018-06-09 11:50:31 +0300

    add images

--- CSA_Project/CSA_Project/Controllers/SettingsController.cs
+++ CSA_Project/CSA_Project/Controllers/SettingsController.cs
@@ -125,5 +125,12 @@ namespace CSA_Project.Controllers
             }
             base.Dispose(disposing);
         }
+        public void BtnDownload_Click(object sender, EventArgs e)
+        {
+            Response.Clear();
+            Response.ContentType = "text/plain";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=foo.txt");
+            Response.Write("some text contents that will be sent to the user");
+        }
     }
 }
