d33db6fbbd9cc57ec3161b8484c63d51d2e85f29
DESKTOP-KD8IF5F\Guy Tordjman <turgibot@gmail.com>
2018-09-17 21:19:50 +0300

    added alert sound to drowsiness

--- CSA_Project/CSA_Project/Controllers/HomeController.cs
+++ CSA_Project/CSA_Project/Controllers/HomeController.cs
@@ -15,16 +15,27 @@ namespace CSA_Project.Controllers
         {
             var setting = DB.Settings.FirstOrDefault();
             var selector = DB.SelectorModels.FirstOrDefault();
+            //general settings
+            string http = "http://";
+            string streamPort = ":8080";
             string host = setting.ServerIP;
-            string video_src = setting.EuclidIP + ":8080" + setting.CameraTopic;
+            string camera_src = http + setting.EuclidIP + streamPort + setting.CameraTopic;
             string server_url = setting.ServerIP;
             string selected_view = selector.SelectedValue;
             string euclid_ip = setting.EuclidIP;
             string euclid_port = setting.EuclidPort;
 
+            //detection settings
 
+            string people_src = http + setting.EuclidIP + streamPort + setting.PeopleTopic;
+            string drowsiness_src = http + setting.EuclidIP + streamPort + setting.DrowsinessTopic;
+            string panic_src = http + setting.EuclidIP + streamPort + setting.PanicTopic;
+            
             int max_people = setting.MaxPeopleAllowed;
-            ViewBag.videoSrc = video_src;
+            ViewBag.cameraSrc = camera_src;
+            ViewBag.PeopleSrc = people_src;
+            ViewBag.DrowsinessSrc = drowsiness_src;
+            ViewBag.PanicSrc = panic_src;
             ViewBag.maxPeople = max_people;
             ViewBag.serverUrl = server_url;
             ViewBag.euclidIP = euclid_ip;
--- CSA_Project/CSA_Project/Scripts/utils.js
+++ CSA_Project/CSA_Project/Scripts/utils.js
@@ -6,47 +6,53 @@ var num_people = document.getElementById('num_people');
 var max_people = document.getElementById('max_people').innerText;
 var alert_txt = 'ALERT!!! Number of people exceeded allowed by ';
 var alert_element = document.getElementById('alert_msg');
+var body_element = document.getElementById('app-body');
+var title_element = document.getElementById('title');
+var status_msg_element = document.getElementById('status_msg');
+var data_element = $('#meta_data');
+var selected_view = data_element.attr('selected_view');
+var server_url = data_element.attr('url');
+var video_src = data_element.attr('video_src');
+var people_src = data_element.attr('people_src');
+var drowsiness_src = data_element.attr('drowsiness_src');
+var panic_src = data_element.attr('panic_src');
+var euclid_ip = data_element.attr('euclid_ip');
+var euclid_port = data_element.attr('euclid_port');
+var audio = document.getElementById('alarm');
 var num_detections = 0;
 var boxes = [];
 var new_data = true;
-var video_src = "";
-var server_url = "";
-var selected_view = "";
-var euclid_ip = "";
-var euclid_port = "";
 var prev_id = 0;
 var isDirty = false;
 var delta = 0;
 var prev_delta = 0;
-
-new_img.setAttribute('id', 'streamer');
+var flick = null;
+var flick_counter = 0;
+var alertStatus = false;
 
 
 $(document).ready(function () {
 
-    var data_element = $('#meta_data');
-    selected_view = data_element.attr('selected_view');
-    server_url = data_element.attr('url');
-    video_src = data_element.attr('video_src');
-    euclid_ip = data_element.attr('euclid_ip');
-    euclid_port = data_element.attr('euclid_port');
+   
+
     post_to_euclid(selected_view, server_url, euclid_ip, euclid_port);
-
-    new_img.setAttribute('src', "http://" + video_src);
-    new_img.setAttribute('width', "640px");
-    new_img.setAttribute('height', "480px");
-
+    
     switch (selected_view) {
         case "People":
+            new_img.setAttribute('src', people_src);
             //ajax get detection information from DB
             var get_people_itrvl = setInterval(getPeople, 250);
             var draw_rect_itrvl = setInterval(drawRect, 33);
             break;
         case "Drowsiness":
-
+            status_msg_element.innerText = "";
+            new_img.setAttribute('src', drowsiness_src);
+            var show_drowsiness_itrvl = setInterval(showDrowsinessStream, 33);
+            var get_drowsiness_itrvl = setInterval(getDrowsiness, 100);
             break;
         case "Panic":
-
+            status_msg_element.innerText = "";
+            new_img.setAttribute('src', panic_src);
             break;
     }
 
@@ -93,7 +99,29 @@ var getPeople = function () {
     })
 }
 
+var getDrowsiness = function () {
+    $.get("http://localhost:53983/api/GetLastDrowsiness", function (data) {
+        if (data.isAwake == false) {
+            if (alertStatus == false) {
+                flicker(true);
+                alertStatus = true;
+            }
+            audio.play();
+            status_msg_element.innerText = "WAKE UP!!!";
+            alert_element.innerText = "HEY!!!!";
 
+        } else {
+            alert_element.innerText = "";
+            status_msg_element.innerText = "";
+            if (alertStatus == true) {
+                flicker(false);
+                alertStatus = false;
+            }
+
+
+        }
+    })
+}
 
 var drawRect = function () {
     ctx.drawImage(new_img, 0, 0);
@@ -111,7 +139,11 @@ var drawRect = function () {
 
 }
 
-
+var showDrowsinessStream = function () {
+    ctx.drawImage(new_img, 0, 0);
+    ctx.beginPath();
+    ctx.stroke();
+}
 var post_to_euclid = function (selected_view, server_url, euclid_ip, euclid_port) {
 
     $.post("http://" + euclid_ip + ":" + euclid_port, {
@@ -119,4 +151,27 @@ var post_to_euclid = function (selected_view, server_url, euclid_ip, euclid_port
         ServerUrl: server_url
     });
 
+}
+var flicker = function (start) {
+    if (start == true) {
+        flick = setInterval(flash, 50);
+    } else {
+        if(flick != null)
+            clearInterval(flick);
+            body_element.setAttribute('style', 'background-color:white;');
+            title_element.setAttribute('style', 'background-color:white;');
+    }
+}
+var flash = function () {
+    var color = makeRandomColor();
+    body_element.setAttribute('style', 'background-color:' + color + ';');
+    title_element.setAttribute('style', 'background-color:' + color + ';');
+}
+
+var makeRandomColor = function(){
+    var c = '';
+    while (c.length < 6) {
+        c += (Math.random()).toString(16).substr(-6).substr(-1)
+    }
+    return '#' + c;
 }
